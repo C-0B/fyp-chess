ce0f8c4ff1b7be727b70406a978ef6848a3bf4d8
Connor <connor_o.b@outlook.com>
2021-12-15 04:49:05 +0000

    refactored bishop move gen to be like all other sliding pieces
    2 for loops, one for a list of unit vectors, another for the magnitude of each vector

--- src/engine_1/pieces/bishop.java
+++ src/engine_1/pieces/bishop.java
@@ -20,100 +20,31 @@ public class bishop extends piece {
 		int[] start_coord = func.sqNumToCoord(startSquare);
 		int[] target_coord = new int[2];
 		
-		for(int index = 1; index <= 8; index++) {// Down and right (up from the perspective of the array)
-			target_coord[0] = start_coord[0] + index;
-			target_coord[1] = start_coord[1] + index;
-			if(this.isOnBoard(target_coord)) {
-				String tgSquare = BOARD[target_coord[0]][target_coord[1]];
-				if(tgSquare.equals(" ")) {
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-				}else if((COLOUR == 1) && (tgSquare.equals(tgSquare.toLowerCase()))){// White Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else if((COLOUR == -1) && (tgSquare.equals(tgSquare.toUpperCase()))) {// Black Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else {
-					break;
-				}
-			}
-		}
+		int[][] moveVectors = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 		
-		for(int index = 1; index <= 8; index++) {// Down and left (up from the perspective of the array)
-			target_coord[0] = start_coord[0] + index;
-			target_coord[1] = start_coord[1] - index;
-			if(this.isOnBoard(target_coord)) {
-				String tgSquare = BOARD[target_coord[0]][target_coord[1]];
-				if(tgSquare.equals(" ")) {
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-				}else if((COLOUR == 1) && (tgSquare.equals(tgSquare.toLowerCase()))){// White Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else if((COLOUR == -1) && (tgSquare.equals(tgSquare.toUpperCase()))) {// Black Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else {
-					break;
-				}
-			}
-		}
-		for(int index = 1; index <= 8; index++) {// Up and right (down from persp of array)
-			target_coord[0] = start_coord[0] - index;
-			target_coord[1] = start_coord[1] + index;
-			if(this.isOnBoard(target_coord)) {
-				String tgSquare = BOARD[target_coord[0]][target_coord[1]];
-				if(tgSquare.equals(" ")) {
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-				}else if((COLOUR == 1) && (tgSquare.equals(tgSquare.toLowerCase()))){// White Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else if((COLOUR == -1) && (tgSquare.equals(tgSquare.toUpperCase()))) {// Black Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else {
-					break;
-				}
-			}
-		}
-		for(int index = 1; index <= 8; index++) {// Up and left (down from persp of array)
-			target_coord[0] = start_coord[0] - index;
-			target_coord[1] = start_coord[1] - index;
-			if(this.isOnBoard(target_coord)) {
-				String tgSquare = BOARD[target_coord[0]][target_coord[1]];
-				if(tgSquare.equals(" ")) {
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-				}else if((COLOUR == 1) && (tgSquare.equals(tgSquare.toLowerCase()))){// White Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else if((COLOUR == -1) && (tgSquare.equals(tgSquare.toUpperCase()))) {// Black Player
-					int targetSquare = func.coordTosqNum(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-					break;
-				}else {
-					break;
+		for(int[] vector : moveVectors) {
+			for(int index = 1; index < 8; index++) {
+				target_coord[0] = start_coord[0] + (vector[0]*index);
+				target_coord[1] = start_coord[1] + (vector[1]*index);
+				if(isOnBoard(target_coord)) {
+					String tgSquare = BOARD[target_coord[0]][target_coord[1]];
+					if(tgSquare.equals(" ")) {
+						int targetSquare = func.coordTosqNum(target_coord);
+						move MOVE = new move(COLOUR, this, startSquare, targetSquare);
+						moves.add(MOVE);
+					}else if((COLOUR == 1) && (tgSquare.equals(tgSquare.toLowerCase()))){// White Player
+						int targetSquare = func.coordTosqNum(target_coord);
+						move MOVE = new move(COLOUR, this, startSquare, targetSquare);
+						moves.add(MOVE);
+						break;
+					}else if((COLOUR == -1) && (tgSquare.equals(tgSquare.toUpperCase()))) {// Black Player
+						int targetSquare = func.coordTosqNum(target_coord);
+						move MOVE = new move(COLOUR, this, startSquare, targetSquare);
+						moves.add(MOVE);
+						break;
+					}else {
+						break;
+					}
 				}
 			}
 		}
