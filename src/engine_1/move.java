f841eb53d18e5ab9898422f49f652b0a7feb80dc
Connor <connor_o.b@outlook.com>
2022-03-16 19:33:14 +0000

    pawnMove class removed, combined with move class fixes #1  i think
    fixes #4

--- src/engine_1/move.java
+++ src/engine_1/move.java
@@ -10,6 +10,7 @@ public class move {
 	int START_SQUARE = 0;
 	int TARGET_SQUARE = 0;
 	String CAPTURED_PIECE = ""; //Empty if no piece captured
+	int EN_PASSANT_SQ = -1;// -1 where no en passant is available, 0-63 where enPassant available
 	
 	
 //	maybe :?
@@ -17,19 +18,37 @@ public class move {
 //	String[][] board;
 //	boolean isPieceTaken;
 	
+	//basic move
 	public move(int playerToMove, piece piece, int startSquare, int targetSquare){
 		PLAYER_TO_MOVE = playerToMove;
 		PIECE = piece;
 		START_SQUARE = startSquare;
 		TARGET_SQUARE = targetSquare;
+		EN_PASSANT_SQ = -1;
 	}
 	
+	// move with capture
 	public move(int playerToMove, piece piece, int startSquare, int targetSquare, String capturePiece){
 		PLAYER_TO_MOVE = playerToMove;
 		PIECE = piece;
 		START_SQUARE = startSquare;
 		TARGET_SQUARE = targetSquare;
+		EN_PASSANT_SQ = -1;
 		CAPTURED_PIECE = capturePiece;
+	}
+	/*
+	public move() {
+		move when a pawn captures another pawn with en passant
+	}
+	*/
+	
+	// double pawn move leaving en passant available
+	public move(int playerToMove, piece piece, int startSquare, int targetSquare, int enPassant){
+		PLAYER_TO_MOVE = playerToMove;
+		PIECE = piece;
+		START_SQUARE = startSquare;
+		TARGET_SQUARE = targetSquare;
+		EN_PASSANT_SQ = enPassant;
 	}
 	
 	/**
@@ -67,7 +86,7 @@ public class move {
 	
 	@Override
 	public String toString() {
-		return PIECE.getNAME()+"@"+START_SQUARE+" -> "+getTARGET_SQUARE()+" \n  "+func.sqNumToStr(this.START_SQUARE)+" -> "+func.sqNumToStr(getTARGET_SQUARE());
+		return PIECE.getNAME()+"@"+START_SQUARE+" -> "+getTARGET_SQUARE()+" \n  "+func.sqNumToStr(this.START_SQUARE)+" -> "+func.sqNumToStr(getTARGET_SQUARE())+" \n En Passant: "+EN_PASSANT_SQ;
 	}
 
 	public int getTARGET_SQUARE() {
