bc059494edf819b4b05284d96690420d4277a4ef
Connor <connor_o.b@outlook.com>
2022-03-16 19:34:55 +0000

    initial test classes
    pawnTest has no use yet
    
    gameTest tests the ability to read a set of fens with no erros and generate a set of fens form a board positiob [2d array], currently does not pass

---
+++ src/test/main/gameTest.java
@@ -0,0 +1,59 @@
+package test.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import engine_1.game;
+
+class gameTest {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new FileReader("listOfFens.txt"))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		    	game GAME = new game(line);
+		    	System.out.println(" real  -> "+line);
+		    	GAME.genFENfromBoard();
+		    	System.out.println("genFEN -> "+GAME.getFEN());
+		    }
+		}catch(IOException E) {
+			System.out.println("Error when testing FEN reading: exception -> "+E);
+		}
+	}
+
+	@Test
+	void readSetOfFENs(){
+		try (BufferedReader br = new BufferedReader(new FileReader("listOfFens.txt"))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		    	game GAME = new game(line);
+		    	System.out.println(line);
+		    }
+		}catch(IOException E) {
+			System.out.println("Error when testing FEN reading: exception -> "+E);
+		}
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	void generateSetOfFENsFromBoard(){
+		try (BufferedReader br = new BufferedReader(new FileReader("listOfFens.txt"))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		    	game GAME = new game(line);
+		    	System.out.println(" real  -> "+line);
+		    	GAME.genFENfromBoard();
+		    	System.out.println("genFEN -> "+GAME.getFEN());
+		    }
+		}catch(IOException E) {
+			System.out.println("Error when testing FEN reading: exception -> "+E);
+		}
+		//fail("Not yet implemented");
+	}
+
+
+}
