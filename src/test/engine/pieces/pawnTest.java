b6b2aff96298b852b393fa32ecff819f65d469d5
Connor <connor_o.b@outlook.com>
2022-04-13 05:40:39 +0100

    moved initial pawnTest into the correct directory
    no tests yet, is just to set the directory correctly

---
+++ src/test/engine/pieces/pawnTest.java
@@ -0,0 +1,14 @@
+package engine.pieces;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class pawnTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
