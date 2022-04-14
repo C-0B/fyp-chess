5069913e3ec3dd071bfd675bd7a88f90501950e2
Connor <connor_o.b@outlook.com>
2022-04-14 04:28:43 +0100

    refactored move.java
    - comments have less lines
    - restructured toString
    - restructured equals()
    - func.sqIntToStr(START_SQUARE);  and targetSquareStr = func.sqIntToStr(TARGET_SQUARE); added to each constructor

--- src/engine/move.java
+++ src/engine/move.java
@@ -9,24 +9,19 @@ public class move {
 	int START_SQUARE = 0;
 	private int TARGET_SQUARE = 0;
 	String CAPTURED_PIECE = ""; //Empty if no piece captured
-	int EN_PASSANT_SQ = -1;// -1 where no en passant is available, 0-63 where enPassant available
-	
-	
-//	maybe :?
-//	float value = 0;//? for evaluation
-//	String[][] board;
-//	boolean isPieceTaken;
-	
+	int EN_PASSANT_SQ = -1;// -1 where no en passant is available, 0-63 where enPassant available	
 	private String startSquareStr;
-	public String getStartSquareStr() {return startSquareStr;}
-	
 	private String targetSquareStr;
-	public String getTargetSquareStr() {return targetSquareStr;}
-
+	
+	/** only constructor that should be used*/
 	public move(String startSquareStr, String targetSquareStr) {
 		this.startSquareStr = startSquareStr;
 		this.targetSquareStr = targetSquareStr;
 	}
+	public int getPLAYER_TO_MOVE() { return PLAYER_TO_MOVE; }
+	public String getStartSquareStr() {return startSquareStr;}
+	public String getTargetSquareStr() {return targetSquareStr;}
+	public int getTARGET_SQUARE() { return TARGET_SQUARE; }
 	
 	
 	//basic move
@@ -41,7 +36,7 @@ public class move {
 	}
 	
 	// move with capture
-	// maybe this is useless - calcualte take pieces from the peices missing on the board
+	// maybe this is useless - calculate take pieces from the pieces missing on the board
 	public move(int playerToMove, piece piece, int startSquare, int targetSquare, String capturePiece){
 		PLAYER_TO_MOVE = playerToMove;
 		PIECE = piece;
@@ -59,14 +54,14 @@ public class move {
 		PLAYER_TO_MOVE = playerToMove;
 		PIECE = piece;
 		START_SQUARE = startSquare;
+		startSquareStr = func.sqIntToStr(START_SQUARE);
 		TARGET_SQUARE = targetSquare;
+		targetSquareStr = func.sqIntToStr(TARGET_SQUARE);
 		EN_PASSANT_SQ = enPassant;
 	}
 	
-	/**
-	 * Checks if the the opponents king is on the target square<p>
-	 * No piece can capture a king
-	 */
+	/** Checks if the the opponents king is on the target square<p>
+	 * No piece can capture a king */
 	public boolean isKingTarget() {
 		if(getPLAYER_TO_MOVE() == 1){// White to move
 			if(CAPTURED_PIECE.equals("k")) {// Black king
@@ -80,37 +75,12 @@ public class move {
 		return false;
 	}
 		
-	/**
-	 *  Compare two moves
-	 */
+	/** Compare this and another move */
 	public boolean equals(move move2) {
-		if(this.getPLAYER_TO_MOVE() == move2.getPLAYER_TO_MOVE()) {
-			if(this.PIECE.equals(move2.PIECE)) {
-				if(this.START_SQUARE == move2.START_SQUARE) {
-					if(this.getTARGET_SQUARE() == move2.getTARGET_SQUARE()) {
-						return true;
-					}
-				}
-			}
-		}
-		return false;
+		return 	this.startSquareStr.equals(move2.startSquareStr) &&
+				this.targetSquareStr.equals(move2.targetSquareStr);
 	}
-	
-//	@Override
-//	public String toString() {
-//		return PIECE.getNAME()+"@"+START_SQUARE+" -> "+getTARGET_SQUARE()+" \n  "+func.sqIntToStr(this.START_SQUARE)+" -> "+func.sqIntToStr(getTARGET_SQUARE())+" \n En Passant: "+EN_PASSANT_SQ;
-//	}
-	
+		
 	@Override
-	public String toString() {
-		return startSquareStr + " -> "+targetSquareStr;
-	}
-
-	public int getTARGET_SQUARE() {
-		return TARGET_SQUARE;
-	}
-
-	public int getPLAYER_TO_MOVE() {
-		return PLAYER_TO_MOVE;
-	}
+	public String toString() { return startSquareStr + " -> "+targetSquareStr; }
 }
