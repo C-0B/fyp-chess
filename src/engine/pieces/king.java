14aa9f47165f620ffd9aa6e062506e506495c282
Connor <connor_o.b@outlook.com>
2022-04-20 21:50:59 +0100

    removed isAdjacentToKing

--- src/engine/pieces/king.java
+++ src/engine/pieces/king.java
@@ -1,17 +1,15 @@
 package engine.pieces;
 
-import java.awt.geom.GeneralPath;
 import java.util.ArrayList;
 
 import chessFunc.func;
+import engine.game;
 import engine.move;
 
 public class king extends piece {
-	boolean inCheck = false;
-
+	
 	public king(String pieceName, int square, int colour) {
 		super(pieceName, square, colour);
-		// TODO Auto-generated constructor stub
 	}
 	@Override
 	public ArrayList<move> generateMoves(String[][] BOARD){
@@ -22,46 +20,6 @@ public class king extends piece {
 		
 		int[][] moveVectors = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, 
 							   {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-								//Castling vectors;
-								// {0, 2}, {2, 0} castling tinleg vectors tempotraily removed 11/4/22 @ 00:20
-		
-		for(int[] vector : moveVectors) {
-			target_coord[0] = start_coord[0] + (vector[0]);
-			target_coord[1] = start_coord[1] + (vector[1]);
-			if(isOnBoard(target_coord)) {
-				String tgSquare = BOARD[target_coord[0]][target_coord[1]];
-				if(tgSquare.equals(" ")) {
-					int targetSquare = func.coordTosqInt(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-				}else if((COLOUR == 1) && (tgSquare.equals(tgSquare.toLowerCase()))){// White Player
-					int targetSquare = func.coordTosqInt(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-				}else if((COLOUR == -1) && (tgSquare.equals(tgSquare.toUpperCase()))) {// Black Player
-					int targetSquare = func.coordTosqInt(target_coord);
-					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
-					moves.add(MOVE);
-				}
-			}
-		}
-		return moves;
-	}
-		
-	/** Used to check if the move would place the players king
-	 *  adjacent to the opponents king
-	 *  
-	 *  this will not be needed as there will be a function in the game
-	 *  to remove all moves that mvoe the king in to squares that are atatcked	 */
-	private boolean isAdjacentToKing(int newCurrSq, String[][] BOARD, int pColour) {
-		// make new king piece with target as current
-		// any of possible move involve capture of king return true
-
-		int[] start_coord = func.sqIntToCoord(newCurrSq);
-		int[] target_coord = new int[2];
-		
-		int[][] moveVectors = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, 
-				   			   {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 		
 		for(int[] vector : moveVectors) {
 			target_coord[0] = start_coord[0] + (vector[0]);
@@ -69,15 +27,18 @@ public class king extends piece {
 			if(isOnBoard(target_coord)) {
 				String tgSquare = BOARD[target_coord[0]][target_coord[1]];
 				int targetSquare = func.coordTosqInt(target_coord);
-				move MOVE = new move(pColour, this, newCurrSq, targetSquare, BOARD[targetSquare/8][targetSquare%8]);
-				if(!tgSquare.equals(" ")) {// If the target is a peice
-					if(MOVE.isKingTarget()) {
-						// If the new target sq is a the opponents king then the king should not have been there is the first place
-						return true;
-					}
+				if(tgSquare.equals(" ")) {
+					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
+					moves.add(MOVE);
+				}else if( (COLOUR == 1) && (tgSquare.equals(tgSquare.toLowerCase())) ){// White Player capture move
+					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
+					moves.add(MOVE);
+				}else if( (COLOUR == -1) && (tgSquare.equals(tgSquare.toUpperCase())) ) {// Black Player capture move
+					move MOVE = new move(COLOUR, this, startSquare, targetSquare);
+					moves.add(MOVE);
 				}
 			}
 		}
-	return false;
+		return moves;
 	}
 }
