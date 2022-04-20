532fa67c01098d809c941717cfa8fec4717fb8e6
Connor <connor_o.b@outlook.com>
2022-04-20 23:05:37 +0100

    functions moved around in file

--- src/opponents/PlayOpponents.java
+++ src/opponents/PlayOpponents.java
@@ -12,36 +12,15 @@ public class PlayOpponents {
 	
 	public static void main(String[] args) {
     	long startTime = System.nanoTime();
-    	int numGames = 8000;
+    	int numGames = 1000;
 		for(int i = 0; i<numGames; i++) {
 			randomVrandom();
+			System.out.println("game played");
 		}
 		long endTime = System.nanoTime();
     	long duration  = (endTime-startTime)/1000000000;
     	System.out.println(numGames+" game(s) computed in "+duration+"s");
 		
-	}
-		
-	public static void checkmateVcheckmate() {
-		game game = new game();
-		System.out.println(game.generateMoves());
-		while( !game.isGameFinshed() ) {
-			System.out.println(game.getPlayerToMove());
-			move checkmateMove = OpponentCheckmate.getCheckmateMove(game);
-			System.out.println(checkmateMove);
-			if( checkmateMove instanceof promotionMove ) {
-				promotionMove pMove = (promotionMove)checkmateMove;
-				game.playMove(pMove, 1, pMove.getPromotionPiece());
-			}else {
-				game.playMove(checkmateMove, 1, "");
-			}
-
-			System.out.println(game.getPlayerToMove());
-			break;
-//			System.out.println(game.getPlayerToMove()+" "+checkmateMove);
-		}
-		logger logger = new logger();
-		logger.logGame("checkmateVcheckmate", game.getEndCondition()+" | "+game.getFEN());
 	}
 	
 	public static void randomVrandom() {
@@ -49,12 +28,6 @@ public class PlayOpponents {
 		ArrayList<move> moves = game.generateMoves();
 		Random random = new Random();
 		while( !game.isGameFinshed() ) {
-			if( moves.size() == 0) {
-				System.out.println("no moves for game");
-				System.out.println(game.getFEN());
-				System.out.println();
-				break;
-			}
 			move randomMove = moves.get( random.nextInt(moves.size()) );
 			if( randomMove instanceof promotionMove ) {
 				promotionMove pMove = (promotionMove)randomMove;
@@ -68,7 +41,6 @@ public class PlayOpponents {
 		logger.logGame("randomVrandom", game.getEndCondition()+" | "+game.getFEN());
 	}
 	
-	
 	public static move getRandomMove(game game) {
 		Random random = new Random();
 		ArrayList<move> moves = game.generateMoves();
@@ -76,5 +48,24 @@ public class PlayOpponents {
 		return randomMove;
 	}
 
-
+	
+	public static void checkmateVcheckmate() {
+		game game = new game();
+		System.out.println(game.generateMoves());
+		while( !game.isGameFinshed() ) {
+			System.out.println(game.getPlayerToMove());
+			move checkmateMove = OpponentCheckmate.getCheckmateMove(game);
+			System.out.println(checkmateMove);
+			if( checkmateMove instanceof promotionMove ) {
+				promotionMove pMove = (promotionMove)checkmateMove;
+				game.playMove(pMove, 1, pMove.getPromotionPiece());
+			}else {
+				game.playMove(checkmateMove, 1, "");
+		}
+		System.out.println(game.getPlayerToMove());
+		break;
+	}
+		logger logger = new logger();
+		logger.logGame("checkmateVcheckmate", game.getEndCondition()+" | "+game.getFEN());
+	}
 }
