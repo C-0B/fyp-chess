3c81ea930925ab03559151044a44bb53f0519aa0
Connor <connor_o.b@outlook.com>
2022-05-01 03:47:49 +0100

    performance comparison String V Integer Logic

--- src/perfomance/LogicVString.java
+++ src/perfomance/LogicVString.java
@@ -2,30 +2,65 @@ package perfomance;
 
 import java.util.Random;
 
+import engineByte.byteGame;
+
 public class LogicVString {
 
 	static String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	static byte[] pieceVals = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12};
 	
 	
 	public static void main(String[] args) {
 		long startTime = System.nanoTime();
 		
-		for(int i = 0; i<1000000; i++) {
+//		for(int i = 0; i<10000000; i++) {
+//			compareByteArr();
+//		}
+			
+		for(int i = 0; i<10000000; i++) {
 			compare8x8String();
 		}
 		
 //		for(int i = 0; i<10000000; i++) {
 //			and64BitLongs();
 //		}
+
 //		for(int i = 0; i<10000000; i++) {
 //			or64BitLongs();
 //		}
-		
 		long endTime = System.nanoTime();
 		long duration = endTime - startTime;
 		System.out.println((duration/1000000)+" ms");
 	}
 	
+	private static boolean compareByteArr() {
+		boolean result = true;
+		
+		byte[][] arr1 = getRandomByteArray();
+		byte[][] arr2 = getRandomByteArray();
+		for(int row = 0; row <8; row++) {
+			for(int column = 0; column <8; column++) {
+				if(arr1[row][column] == arr2[row][column]) {
+					result = !result;
+				}
+			}
+		}
+		return result;
+	}
+	
+	private static byte[][] getRandomByteArray(){
+		byte[][] randomArr = new byte[8][8]; 
+		Random random = new Random();
+		for(int row = 0; row <8; row++) {
+			random.nextBytes(randomArr[row]);
+			for(int column= 0; column <8; column++) {
+//				System.out.print(randomArr[row][column]+" ");
+			}
+//			System.out.println();
+		}
+		return randomArr;
+	}
 	
 	private static long or64BitLongs() {
 		long result = 0;
@@ -38,6 +73,9 @@ public class LogicVString {
 		long result = 0;
 		long l1 = getRandom64Bit();
 		long l2 = getRandom64Bit();
+		if(l1 < l2) {
+			return result;
+		}
 		result = l1 & l2;
 		return result;
 	}
