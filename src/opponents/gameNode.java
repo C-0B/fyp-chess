6d7a135be1b578c05a1c703f1194dda40a1f16b3
Connor <connor_o.b@outlook.com>
2022-05-01 03:50:00 +0100

    minimax implemented in constructor

--- src/opponents/gameNode.java
+++ src/opponents/gameNode.java
@@ -13,6 +13,9 @@ public class gameNode {
 	boolean isLeafNode = false;
 	int PLAYERTOMOVE;
 	
+	move moveToPlay = null;
+	move lastMovePlayed = null;
+	
 	// Immediate subnodes from this node.
 	// One for each legal move a resulting following posiiton
 	ArrayList<gameNode> subNodes = new ArrayList<gameNode>();
@@ -20,20 +23,16 @@ public class gameNode {
 	
 	public static void main(String[] args) {
 		long startTime = System.nanoTime();
-		game rootGame = new game();
-		int maxTreeDepth = 2;
+		game rootGame = new game("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+		int maxTreeDepth = 3;
 		
-		gameNode tree = new gameNode(rootGame, 0, maxTreeDepth);
+		gameNode tree = new gameNode(rootGame, 0, maxTreeDepth, null);
 		
 		long endTime = System.nanoTime();
-		long duration  = (endTime-startTime)/1000000000;
-    	System.out.println("tree created in nodes counted in "+duration+"s");
-    	
-    	System.out.println(tree.getTotalPossibleGames());
-		System.out.println();
-		
-//		long numSubNodes = tree.getTotalPossibleGames();
-//    	System.out.println(numSubNodes+" games counted in "+duration+"s");    
+		long duration  = (endTime-startTime)/1000000;
+    	System.out.println(tree.getTotalPossibleGames()+" games counted in "+duration+" ms");
+    	System.out.println(tree.evaluation);
+    	System.out.println(tree.moveToPlay);
 	}
 	
 	/**
@@ -42,13 +41,15 @@ public class gameNode {
 	 * @param DEPTH current depth, starts at 0
 	 * @param MAXDEPTH maximum depth of the tree.
 	 */
-	public gameNode(game GAME, int DEPTH, int MAXDEPTH) {
+	public gameNode(game GAME, int DEPTH, int MAXDEPTH, move lastMove) {
 		game = GAME;
 		depth = DEPTH;
 		maxDepth = MAXDEPTH;
 		PLAYERTOMOVE = game.getPlayerToMove();
 		legalMoves = game.generateMoves();
+		lastMovePlayed = lastMove;
 		
+		//Create the tree
 		if ( !(game.isGameFinshed()) ) {//if the has possible moves to be played
 			if( depth < MAXDEPTH ) {
 				for(move move :legalMoves) { // create all subnodes (0 legal moves if game is finished)
@@ -62,86 +63,71 @@ public class gameNode {
 						subGame.playMove(move, 1, "");
 					}
 					
-					gameNode subNode = new gameNode(subGame, (this.depth+1), maxDepth);
-					//subNodes.add(subNode);
+					gameNode subNode = new gameNode(subGame, (this.depth+1), maxDepth, move);
+					subNodes.add(subNode);
 				}
-			}
-		}
-	}
-	
-	/**
-	 * Minimax algorithm to evaluate a tree of gameNode objects with 
-	 * @param rootNode
-	 * @param currentDepth
-	 * @param maxDepth
-	 * @return evaluation
-	 */
-	public int minimaxGame(gameNode rootNode, int currentDepth, int maxDepth) {
-		if ( (rootNode.game.isGameFinshed()) ||
-			 (currentDepth == maxDepth) ) {
-			// static eval of board
-			// return static eval
-		}
-		if ( game.getPlayerToMove() == 1 ) { // white to play, max
-			int maximumEval = Integer.MIN_VALUE;
-			for(gameNode subGameNode : subNodes) {
-				int evaluation = minimaxGame(subGameNode, currentDepth+1, maxDepth);
-				maximumEval = Math.max(maximumEval, evaluation);
-			}
-			return maximumEval;
-		} else if (game.getPlayerToMove() == -1) { // black to play, min
-			int minimumEval = Integer.MAX_VALUE;
-			for(gameNode subGameNode : subNodes) {
-				int evaluation = minimaxGame(subGameNode, currentDepth+1, maxDepth);
-				minimumEval = Math.min(minimumEval, evaluation);
-			}
-			return minimumEval;
-		}
+			}else { isLeafNode = true; }// Max depth reached
+		}else { isLeafNode = true; }// Game has ended 
 		
-		return 0;
+		// Evaluate the tree
+		evaluation = this.evaluate();
 	}
-	
-	/**
-	 * Minimax algorithm with alpha beta pruning to
-	 * increase performance
-	 * @param rootNode
-	 * @param currentDepth
-	 * @param maxDepth
-	 * @param alpha
-	 * @param beta
-	 * @return the final evaluation
-	 */
-	public int alphaBetaPruning(gameNode rootNode, int currentDepth, int maxDepth, int alpha, int beta) {
-		if ( (rootNode.game.isGameFinshed()) ||
-				 (currentDepth == maxDepth) ) {
-				// static eval of board
-				// return static eval
+/*
+ * -----------------------------------------------------------------------------
+ */
+	public int evaluate() {
+		int EVALUATION = 0;
+		
+		if(game.isGameFinshed()) {// win / loss / draw
+			if(game.getEndCondition().equals("white win")) {
+				EVALUATION = Integer.MAX_VALUE;
+			}else if(game.getEndCondition().equals("black win")){
+				EVALUATION = Integer.MIN_VALUE;
+			}else {// draw or stalemate or 50 move limit
+				EVALUATION = 0;
 			}
-			if ( game.getPlayerToMove() == 1 ) { // white to play, max
-				int maximumEval = Integer.MIN_VALUE;
-				for(gameNode subGameNode : subNodes) {
-					maximumEval = alphaBetaPruning(subGameNode, currentDepth+1, maxDepth, alpha, beta);
-					if (maximumEval >= beta) {
-						break;
-					}
-					alpha = Math.max(alpha, maximumEval);
+			
+		}else {//game not finished
+			if( this.isLeafNode ) { // no subnodes
+				EVALUATION = eval.getPieceEval(this.game.getBoard());
+			}else { // Has subnodes
+				if(PLAYERTOMOVE == 1) {//white to move
+					EVALUATION = maximumEval();
+				}else {//black to move
+					EVALUATION = minimumEval();
 				}
-				return maximumEval;
-			} else if (game.getPlayerToMove() == -1) { // black to play, min
-				int minimumEval = Integer.MAX_VALUE;
-				for(gameNode subGameNode : subNodes) {
-					minimumEval = alphaBetaPruning(subGameNode, currentDepth+1, maxDepth, alpha, beta);
-					if (minimumEval <= alpha) {
-						break;
-					}
-					beta = Math.min(beta, minimumEval);
-				}
-				return minimumEval;
 			}
-			return 0;
+		}
+		return EVALUATION;
 	}
 	
 	
+	public int minimumEval() {
+		int minEval = Integer.MAX_VALUE;
+		for(gameNode node : subNodes ) {
+			int curMoveEval = node.evaluate();
+			if (minEval >= curMoveEval) {
+				minEval  = curMoveEval;
+				moveToPlay = node.lastMovePlayed;
+			}
+		}
+		return minEval;
+	}
+	
+	public int maximumEval() {
+		int maxEval = Integer.MIN_VALUE;
+		for(gameNode node :subNodes) {
+			int curMoveEval = node.evaluate();
+			if (maxEval <= curMoveEval) {
+				maxEval  = curMoveEval;
+				moveToPlay = node.lastMovePlayed;
+			}
+		}
+		return maxEval;
+	}
+/*
+ * -----------------------------------------------------------------------------
+ */
 	public int getTotalPossibleGames() {
 		if ( subNodes.size() == 0) {
 			return 1;
@@ -165,57 +151,5 @@ public class gameNode {
 			sum += 1;// count all nodes in the tree
 			return sum;
 		}
-	}
-/*
- * -----------------------------------------------------------------------------
- */
-	public int evaluate() {
-		int EVALUATION = 0;
-		
-		if(game.isGameFinshed()) {// win / loss / draw
-			if(game.getEndCondition().equals("white win")) {
-				EVALUATION = Integer.MAX_VALUE;
-			}else if(game.getEndCondition().equals("black win")){
-				EVALUATION = Integer.MIN_VALUE;
-			}else {// draw or stalemate or 50 move limit
-				EVALUATION = 0;
-			}
-			
-		}else {//game not finished
-			if( depth >= maxDepth) { // no subnodes
-				//eval unfinished position
-				//eval function
-			}else { // has subnodes
-				if(PLAYERTOMOVE == 1) {//white to move
-					EVALUATION = maximumEval();
-				}else {//black to move
-					EVALUATION = minimumEval();
-				}
-			}
-		}
-		return EVALUATION;
-	}
-	
-	
-	public int minimumEval() {
-		int minEval = Integer.MAX_VALUE;
-		for(gameNode node : subNodes ) {
-			int curMoveEval = node.evaluate();
-			if (curMoveEval >= minEval) {
-				minEval = curMoveEval;
-			}
-		}
-		return minEval;
-	}
-	
-	public int maximumEval() {
-		int maxEval = Integer.MIN_VALUE;
-		for(gameNode node :subNodes) {
-			int curMoveEval = node.evaluate();
-			if (curMoveEval >= maxEval) {
-				maxEval = curMoveEval;
-			}
-		}
-		return maxEval;
 	}
 }
